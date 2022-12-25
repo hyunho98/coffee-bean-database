fb7a2d188656aa6527d0301360964527a6bb89bb
Alex Lee <hyunho98@gmail.com>
2022-12-25 13:15:25 -0500

    added function to handle coffee creation in the dom

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 document.addEventListener("DOMContentLoaded", (e) => {
     coffeeForm = document.querySelector('.add-coffee-form')
     inputs = coffeeForm.querySelectorAll('.input-text')
-
     coffeeForm.addEventListener('submit', (e) => {})
 
     onsubmit = (e) => {
@@ -23,6 +22,24 @@ document.addEventListener("DOMContentLoaded", (e) => {
         })
         .then((response) => response.json())
         .then((data) => {
+            console.log(data)
         })
     }
 })
+
+function createTile(beanObj) {
+    const tile = document.createElement('div')
+    const locationh3 = document.createElement('h3')
+    const nameh3 = document.createElement('h3')
+    const p = document.createElement('p')
+    const h4 = document.createElement('h4')
+    tile.className = 'tile'
+    locationh3.innerText = beanObj.location
+    locationh3.className = 'location'
+    nameh3.innerText = beanObj.name
+    nameh3.className = 'name'
+    p.innerText = beanObj.taste
+    h4.value = beanObj.rating
+    tile.append(locationh3, nameh3, p, h4)
+    return tile;
+  }
