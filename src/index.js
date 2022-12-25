792b30fd669e4ddc9000f9db29c8d5703207ca11
Alex Lee <hyunho98@gmail.com>
2022-12-25 13:02:34 -0500

    added ratings and post method to form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-fetch('http://localhost:3000/coffee_beans')
-.then((response) => response.json())
-.then((data) => {
-    console.log(data)
-})
-
 //list based on location in the db.json
+document.addEventListener("DOMContentLoaded", (e) => {
+    coffeeForm = document.querySelector('.add-coffee-form')
+    inputs = coffeeForm.querySelectorAll('.input-text')
+
+    coffeeForm.addEventListener('submit', (e) => {})
+
+    onsubmit = (e) => {
+        e.preventDefault()
+
+        fetch('http://localhost:3000/coffee_beans', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({
+                'location': `${inputs[0].value}`,
+                'name': `${inputs[1].value}`,
+                'taste': `${inputs[2].value}`,
+                'rating': 0
+            })
+        })
+        .then((response) => response.json())
+        .then((data) => {
+        })
+    }
+})
